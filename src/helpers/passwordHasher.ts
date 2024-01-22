fc0f9424c592c0149b33999453ecb3f72f80f542
shahidx0x <dev.imshahid@gmail.com>
2024-01-23 00:34:38 +0600

    update .3e3d6b6f7b70ae46acc401e7f7bd83fcc0797311-audit.json, .0bd05ebf8bd60b6f677475d131d56f4db2475688-audit.json and passwordHasher.ts

--- src/helpers/passwordHasher.ts
+++ src/helpers/passwordHasher.ts
@@ -2,7 +2,6 @@ import bcrypt from 'bcrypt';
 
 export async function passwordHaser(password: string): Promise<string> {
   const hashedPassword = await bcrypt.hash(password, 10);
-  console.log(hashedPassword);
 
   return hashedPassword;
 }
